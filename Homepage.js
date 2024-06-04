185d61269c89532b56671a70f31e9b2e089f02b0
edumatrix_dev <eduwebdev@gmail.com>
2024-06-04 15:30:13 +0700

    done subpage program

--- Homepage.js
+++ Homepage.js
@@ -63,7 +63,9 @@ const slide = document.querySelector(".carousel-slide");
 
 function handleCarouselMove(positive = true) {
   const slideWidth = slide.clientWidth;
-  carousel.scrollLeft = positive ? carousel.scrollLeft + slideWidth : carousel.scrollLeft - slideWidth;
+  carousel.scrollLeft = positive
+    ? carousel.scrollLeft + slideWidth
+    : carousel.scrollLeft - slideWidth;
 }
 
 // next.addEventListener('click', handleScrollNext)
@@ -74,4 +76,36 @@ document.querySelectorAll(".accordion-item").forEach((item) => {
   item.querySelector(".accordion-item-header").addEventListener("click", () => {
     item.classList.toggle("open");
   });
+});
+
+
+// MASTER TEACHER
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
 });
---
+++ Pageprogram/ProgramAkurasi/ProgramAkurasi.js
@@ -0,0 +1,9 @@
+// NAVBAR
+let nav = document.querySelector("nav");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
